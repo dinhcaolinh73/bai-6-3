b55ead248e47f20a5ce261b19fa0479ee90e4011
dinhcaolinh73 <156870561+dinhcaolinh73@users.noreply.github.com>
2024-03-06 09:27:07 +0700

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,21 @@
+package Person;
+
+public class Student extends Person {
+    double gpa;
+    public Student(String name, int age, String address){
+        super(name, age, address);
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public void display(){
+        super.display();
+        System.out.println("GPA:" + this.gpa);
+    }
+}
